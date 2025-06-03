aa09ca73d4993f6bcb3b5459235c68928d32ae2a
NoahDevQuest <nrda1991@gmail.com>
2025-06-03 16:24:15 -0700

    Update Program.cs
    Fully functional and tested patch.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System;
+using PasswordGenerator;
 
 namespace PasswordGenerator
 {
@@ -9,7 +10,26 @@ namespace PasswordGenerator
         
         static void Main()
         {
-            
+            Console.WriteLine("Welcome to my password Generator.");
+
+            MainForm PasswordForm = new MainForm();
+
+            PasswordGeneratorLogic PasswordGenerator = new PasswordGeneratorLogic();
+
+            PasswordForm.FillingForm();
+
+            PasswordGenerator.RemainingLength = PasswordForm.PasswordLength;
+            PasswordGenerator.TrueLength = PasswordForm.PasswordLength;
+
+            PasswordGenerator.SCIncluded = PasswordForm.SpecialCharacters;
+            PasswordGenerator.ULIncluded = PasswordForm.UppercaseLetters;
+            PasswordGenerator.LLIncluded = PasswordForm.LowercaseLetters;
+            PasswordGenerator.NIncluded = PasswordForm.Numbers;
+            PasswordGenerator.FLWIncluded = PasswordForm.FourLetterWords;
+
+            PasswordGenerator.PasswordGenerator();
+
+            Console.WriteLine("Thanks for using my password generator.");
         }
     }
 }
