f15f3b78793cad2500d282089dd55b2108dc1ca9
NoahDevQuest <nrda1991@gmail.com>
2025-06-03 16:23:44 -0700

    Update PasswordGeneratorLogic.cs
    Fully functional and tested patch.

--- PasswordGeneratorLogic.cs
+++ PasswordGeneratorLogic.cs
@@ -3,10 +3,153 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PasswordGenerator;
 
 namespace PasswordGenerator
 {
     class PasswordGeneratorLogic
     {
+        public string SpecialCharacters;
+        public string UpperCaseLetters;
+        public string LowerCaseLetters;
+        public int[] Numbers;
+        public string[] FourLetterWords;
+
+        public bool SCIncluded;
+        public bool ULIncluded;
+        public bool LLIncluded;
+        public bool NIncluded;
+        public bool FLWIncluded;
+
+        public int TrueLength;
+        public int RemainingLength;
+        public string NewPassword;
+        public int PasswordStrength;
+
+        public PasswordGeneratorLogic()
+        {
+            SpecialCharacters = "~`!@#$%^&*()_-=|[]{};:'.>/?";
+            UpperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            LowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+            Numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+            FourLetterWords = ["Tree", "Rock", "Wind", "Book", "Code", "Desk", "Lamp", "Note", "Void", "Fang", "Wisp", "Grim", "Husk", "Bane", "Claw", "Lurk", "Byte", "Ping", "Node", "Hash", "Port", "Data", "Boot", "Loop"];
+
+            NewPassword = "";
+        }
+
+
+        public void PasswordGenerator()
+        {
+            Random rng = new Random();
+
+            Console.WriteLine("Generating...");
+            while (this.RemainingLength > 4)
+            {
+                int randomChoice = rng.Next(0, 5);
+                
+                if (SCIncluded == true && randomChoice == 0)
+                {
+                    this.NewPassword += this.SpecialCharacters[rng.Next(0, this.SpecialCharacters.Length)];
+                    this.RemainingLength -= 1;
+                }
+                else if (ULIncluded == true && randomChoice == 1)
+                {
+                    this.NewPassword += this.UpperCaseLetters[rng.Next(0, this.UpperCaseLetters.Length)];
+                    this.RemainingLength -= 1;
+                }
+                else if (LLIncluded == true && randomChoice == 2)
+                {
+                    this.NewPassword += this.LowerCaseLetters[rng.Next(0, this.LowerCaseLetters.Length)];
+                    this.RemainingLength -= 1;
+                }
+                else if (NIncluded == true && randomChoice == 3)
+                {
+                    this.NewPassword += this.Numbers[rng.Next(0, this.Numbers.Length)];
+                    this.RemainingLength -= 1;
+                }
+                else if (this.FLWIncluded == true && randomChoice == 4)
+                {
+                    this.NewPassword += this.FourLetterWords[rng.Next(0, this.FourLetterWords.Length)];
+                    this.RemainingLength -= 4;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            while (this.RemainingLength > 0)
+            {
+                int randomChoice = rng.Next(0, 4);
+
+                if (SCIncluded == false && ULIncluded == false && LLIncluded == false && NIncluded == false)
+                {
+                    this.RemainingLength = 0;
+                    Console.WriteLine("Password length shortened because of only using four letter words.");
+                }
+                if (SCIncluded == true && randomChoice == 0)
+                {
+                    this.NewPassword += this.SpecialCharacters[rng.Next(0, this.SpecialCharacters.Length)];
+                    this.RemainingLength -= 1;
+                }
+                else if (ULIncluded == true && randomChoice == 1)
+                {
+                    this.NewPassword += this.UpperCaseLetters[rng.Next(0, this.UpperCaseLetters.Length)];
+                    this.RemainingLength -= 1;
+                }
+                else if (LLIncluded == true && randomChoice == 2)
+                {
+                    this.NewPassword += this.LowerCaseLetters[rng.Next(0, this.LowerCaseLetters.Length)];
+                    this.RemainingLength -= 1;
+                }
+                else if (NIncluded == true && randomChoice == 3)
+                {
+                    this.NewPassword += this.Numbers[rng.Next(0, this.Numbers.Length)];
+                    this.RemainingLength -= 1;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            if (this.TrueLength >= 8 && this.TrueLength < 16)
+            {
+                this.PasswordStrength += 1;
+            }
+            else if (this.TrueLength >= 16 && this.TrueLength < 24)
+            {
+                this.PasswordStrength += 2;
+            }
+            else if (this.TrueLength >= 24)
+            {
+                this.PasswordStrength += 3;
+            }
+            if (this.SCIncluded == true)
+            {
+                this.PasswordStrength += 1;
+            }
+            if (this.ULIncluded == true)
+            {
+                this.PasswordStrength += 1;
+            }
+            if (this.LLIncluded == true)
+            {
+                this.PasswordStrength += 1;
+            }
+            if (this.NIncluded == true)
+            {
+                this.PasswordStrength += 1;
+            }
+            if (this.FLWIncluded == true)
+            {
+                this.PasswordStrength += 1;
+            }
+            
+            Console.WriteLine(this.NewPassword);
+            Console.WriteLine("Password stength is: " + this.PasswordStrength + "/8");
+        }
     }
+
+    
 }
